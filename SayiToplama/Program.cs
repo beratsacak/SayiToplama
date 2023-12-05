ad60fb8a7f80c6329ddd8cc8a9de97ded844c7d6
berat584 <berat584@BPC08>
2023-12-05 09:25:52 +0300

    Proje dosyası ekle.

---
+++ SayiToplama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiToplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            İslem islem = new İslem();
+            double sonuc =islem.Topla(45,45,85,54,785,485,485);
+            Console.WriteLine(sonuc);
+        }
+    }
+    public class İslem
+    {
+        public double Topla(params double[] sayilar)
+        {
+            double toplam = 0;
+            foreach (var t in sayilar)
+            {
+                toplam += t;
+            }
+            return toplam;
+        }
+    }
+}
